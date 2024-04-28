03504ca6482bf419823d84903b2748fc31d553f4
prumernyjonas <zetkoc@gmail.com>
2024-04-28 14:58:54 +0200

    Svetlo, oprava

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,12 +1,11 @@
 using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement; // Přidáme pro práci s scénami
-
 public class Player : MonoBehaviour
 {
     public int hp;
     public int maxHP = 100;
-
+    
     public string nextSceneName = "DeathScene"; // Název scény, na kterou se přejde po smrti
 
     void Start()
