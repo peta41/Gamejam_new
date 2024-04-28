88ab2cce3540b629c2fc56d59eba288dff741fe3
peta_3 <pdoceky@gmail.com>
2024-04-28 13:11:17 +0200

    ButtonsFuctionality

---
+++ Assets/Scripts/LlamaSelected.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LlamaSelected : MonoBehaviour
+{
+    public void LlamaSelect(int sceneIndex)
+    {
+        SceneManager.LoadScene(4);
+    }
+}
